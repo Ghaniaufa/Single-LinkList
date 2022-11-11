71472e102b5cf6c5ca6bdaecb40e2e3cd2ccedec
Ghaniaufa <maula.ghani.ft21@mail.umy.ac.id>
2022-11-11 17:10:26 +0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace singly_linked_list
+{
+        // each node consist of the information part and lik to the next mode
+
+     class Node
+    {
+        public int rollNumber;
+        public string name;
+        public Node next;
+    }
+
+    
+}
